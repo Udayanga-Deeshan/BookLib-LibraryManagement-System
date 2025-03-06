6bc3a237e89ba46ea40e38d52bee002fc3af552b
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-03-06 14:51:33 +0530

    created dto & entity for librarian

---
+++ src/main/java/controller/RegisterFormController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class RegisterFormController {
+
+    @FXML
+    private JFXPasswordField txtConfirmPassword;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+
+    }
+
+}
---
+++ src/main/java/dto/Librarian.java
@@ -0,0 +1,20 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Librarian {
+
+    private  String username;
+
+    private  String email;
+
+    private String password;
+
+}
